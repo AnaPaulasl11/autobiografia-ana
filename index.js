dd18b249f1360ca7373f679e253808f752697e00
AnaPaulasl11 <anapaulalima1901@gmail.com>
2025-05-18 18:16:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function mudaCor(link) {
+    link.style.color= '#7eb9c4';
+}
+
+function restaurarCor(link) {
+    link.style.color= '#000';
+}
+
+function verMensagem() {
+    alert("Meus agradecimentos, obrigada pela visita!");
+}
+
+function verMensagem() {
+    document.getElementById("mensagem").innerText="Meus agradecimentos, obrigada pela visita!🚀✨"
+}
